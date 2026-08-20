0fe25c170a51063e0820eff9efa482c6e34cdc13
ZeynepKara06 <zeynep.karamustafao1@stu.pirireis.edu.tr>
2026-08-20 15:28:24 +0300

    Update pomodoro_timer1.py

--- pomodoro_timer1.py
+++ pomodoro_timer1.py
@@ -3,8 +3,8 @@ while True:
     try:
         our_worktime= float(input("write the work hour you want? : "))
 
-if  0 <= our_worktime <= 10:            #girilen saati kontrol ediyoruz
- total_seconds = int(our_worktime * 3600)      #verilen saniye ye çeviriyorum sistem saniye üzerinden diye 
+if  0 <= our_worktime <= 10:            #checking if the input is between 0 and 10 hours
+ total_seconds = int(our_worktime * 3600)      #converting the input to seconds
 else:
  print(input("Write hour between 0 and 10 numbers: "))
 
@@ -13,7 +13,7 @@ def countdown (seconds):
   hours = seconds // 3600
   minutes = (seconds % 3600) // 60
   secs = seconds % 60
-  print(f"{hours:02d}:{minutes:02d}:{secs:02d}" , end="\r")     #sayıları 02:30:55 gibi yazıcak.
+  print(f"{hours:02d}:{minutes:02d}:{secs:02d}" , end="\r")     
   time.sleep(1)
   seconds -= 1
 
@@ -21,7 +21,7 @@ work_minutes = 25
 break_minutes = 5
 work = work_minutes * 60
 break_time= break_minutes * 60
-pomodoro_count = total_seconds // (work + break_time)       #break ile beraber kaç pomodoro olucak onu sayıyoruz.
+pomodoro_count = total_seconds // (work + break_time)       #calculation of how many pomodoros can be completed in the given time
 
 for i in range(pomodoro_count):
  print(f"\n Pomodoro {i+1} started")
